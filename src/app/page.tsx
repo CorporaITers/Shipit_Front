1d0526cc6d876d242f167c7ae6d531589bf81ca4
Yoshio Nomura <nomurin.alex103@gmail.com>
2025-04-20 15:46:08 +0900

    13th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
   const handleFeedback = async (index: number, value: "yes" | "no") => {
     const schedule = results[index];
     try {
-      await fetch("http://localhost:8000/update-feedback", {
+      await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + 'update-feedback', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
