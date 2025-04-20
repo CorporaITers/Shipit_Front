9aba15a82e2369825c33d37ac7696770b066a63e
Yoshio Nomura <nomurin.alex103@gmail.com>
2025-04-20 13:53:31 +0900

    7th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
     }
 
     try {
-      const res = await fetch("http://localhost:8000/recommend-shipping", {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/recommend-shipping', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
